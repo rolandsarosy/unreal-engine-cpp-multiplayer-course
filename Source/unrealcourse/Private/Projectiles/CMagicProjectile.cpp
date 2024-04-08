b9d33d9f947ef1f1846935156c18251069d5b8ee
rolandsarosy <roland.sarosy@gmail.com>
2024-04-08 13:35:01 +0200

    Resolve issue where the basic magic projectile collided with the caster pawn

--- Source/unrealcourse/Private/Projectiles/CMagicProjectile.cpp
+++ Source/unrealcourse/Private/Projectiles/CMagicProjectile.cpp
@@ -19,7 +19,7 @@ void ACMagicProjectile::PostInitializeComponents()
 void ACMagicProjectile::OnActorOverlap(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep,
                                        const FHitResult& SweepResult)
 {
-	if (OtherActor)
+	if (OtherActor && OtherActor != GetInstigator())
 	{
 		UCAttributeComponent* AttributeComponent = Cast<UCAttributeComponent>(OtherActor->GetComponentByClass(UCAttributeComponent::StaticClass()));
 
