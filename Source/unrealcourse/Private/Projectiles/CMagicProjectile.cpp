65db560ef292d98abd396cc8f87055bacfea19db
rolandsarosy <roland.sarosy@gmail.com>
2024-04-10 18:29:00 +0200

    Add audio behaviour to MagicProjectile

--- Source/unrealcourse/Private/Projectiles/CMagicProjectile.cpp
+++ Source/unrealcourse/Private/Projectiles/CMagicProjectile.cpp
@@ -1,14 +1,19 @@
 #include "Projectiles/CMagicProjectile.h"
 
 #include "CAttributeComponent.h"
+#include "Components/AudioComponent.h"
 #include "Components/SphereComponent.h"
 #include "GameFramework/ProjectileMovementComponent.h"
 #include "Kismet/GameplayStatics.h"
+#include "Sound/SoundCue.h"
 
 ACMagicProjectile::ACMagicProjectile()
 {
 	ProjectileMovementComponent->InitialSpeed = 2000;
 	ProjectileMovementComponent->ProjectileGravityScale = 0.0f;
+
+	LoopingAudioComponent = CreateDefaultSubobject<UAudioComponent>("LoopingAudioComponent");
+	LoopingAudioComponent->SetupAttachment(RootComponent);
 
 	DamageAmount = 20.0f;
 }
@@ -19,18 +24,23 @@ void ACMagicProjectile::PostInitializeComponents()
 	SphereComponent->OnComponentHit.AddDynamic(this, &ACMagicProjectile::OnComponentHit);
 }
 
+void ACMagicProjectile::BeginPlay()
+{
+	Super::BeginPlay();
+}
+
 void ACMagicProjectile::OnComponentHit(UPrimitiveComponent* HitComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, FVector NormalImpulse, const FHitResult& Hit)
 {
 	if (OtherActor && OtherActor != GetInstigator())
 	{
-		
 		// If the target has an AttributeComponent attached, damage it.
 		if (UCAttributeComponent* AttributeComponent = Cast<UCAttributeComponent>(OtherActor->GetComponentByClass(UCAttributeComponent::StaticClass())))
 		{
-			AttributeComponent -> ApplyHealthChange(-DamageAmount);
+			AttributeComponent->ApplyHealthChange(-DamageAmount);
 		}
 
 		UGameplayStatics::SpawnEmitterAtLocation(GetWorld(), ImpactParticleSystem, GetActorLocation(), GetActorRotation());
+		UGameplayStatics::PlaySoundAtLocation(GetWorld(), Cast<USoundBase>(ImpactSoundCue), GetActorLocation(), GetActorRotation());
 		Destroy();
 	}
 }
--- Source/unrealcourse/Public/Projectiles/CMagicProjectile.h
+++ Source/unrealcourse/Public/Projectiles/CMagicProjectile.h
@@ -4,6 +4,10 @@
 #include "Projectiles/CBaseProjectile.h"
 #include "CMagicProjectile.generated.h"
 
+class USoundCue;
+class UAudioComponent;
+class UParticleSystem;
+
 UCLASS()
 class UNREALCOURSE_API ACMagicProjectile : public ACBaseProjectile
 {
@@ -14,10 +18,18 @@ public:
 	
 	virtual void PostInitializeComponents() override;
 
+	virtual void BeginPlay() override;
+
 protected:
 	UFUNCTION()
 	void OnComponentHit(UPrimitiveComponent* HitComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, FVector NormalImpulse, const FHitResult& Hit);
 
+	UPROPERTY(EditDefaultsOnly, Category="Audio")
+	TObjectPtr<UAudioComponent> LoopingAudioComponent;
+	
+	UPROPERTY(EditDefaultsOnly, Category="Audio")
+	TObjectPtr<USoundCue> ImpactSoundCue;
+	
 	UPROPERTY(EditDefaultsOnly, Category="Effects")
 	TObjectPtr<UParticleSystem> ImpactParticleSystem; 
 	
