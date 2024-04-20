7b3c5c78e2a6ae87a55e7dfdd9ee8bc573720aea
rolandsarosy <roland.sarosy@gmail.com>
2024-04-20 21:56:28 +0200

    Refactor and clean up RangedAttack BTTask

--- Source/unrealcourse/Public/AI/CBTTask_RangedAttack.h
+++ Source/unrealcourse/Public/AI/CBTTask_RangedAttack.h
@@ -12,15 +12,16 @@ class UNREALCOURSE_API UCBTTask_RangedAttack : public UBTTaskNode
 	virtual EBTNodeResult::Type ExecuteTask(UBehaviorTreeComponent& OwnerComp, uint8* NodeMemory) override;
 
 protected:
-	UPROPERTY(EditAnywhere, Category="Blackboard Keys")
+	UPROPERTY(EditAnywhere, Category="Blackboard Keys", meta=(Tooltip = "This Blackboard key represents the target to make the ranged attack towards."))
 	FBlackboardKeySelector TargetActorKey;
 
-	UPROPERTY(EditAnywhere, Category="Projectile")
+	UPROPERTY(EditAnywhere, Category="Projectile", meta=(Tooltip = "This is the Projectile that'll be spawned when making the ranged attack towards the target."))
 	TSubclassOf<AActor> ProjectileClass;
 
-	UPROPERTY(EditAnywhere, Category="Projectile")
+	UPROPERTY(EditAnywhere, Category="Projectile", meta=(Tooltip = "This is the Skeletal Mesh Socket's name where the projectile will spawn from, towards the target."))
 	FName AttackSocketName;
-	
+
+private:
 	AActor* GetTargetActor(const UBehaviorTreeComponent& OwnerComp) const;
 
 	static ACharacter* GetAICharacter(const UBehaviorTreeComponent& OwnerComp);
