15677fb599b89b40298a1b3a9ca7e703f2167fd8
rolandsarosy <roland.sarosy@gmail.com>
2024-05-13 19:08:22 +0200

    Add world interaction UI feedback

--- Source/unrealcourse/Private/Components/CInteractionComponent.cpp
+++ Source/unrealcourse/Private/Components/CInteractionComponent.cpp
@@ -2,34 +2,90 @@
 
 #include "CGameplayInterface.h"
 #include "DrawDebugHelpers.h"
+#include "Blueprint/UserWidget.h"
+#include "CWorldUserWidget.h"
 
 static TAutoConsoleVariable CVarDebugDrawInteraction(TEXT("course.DebugDrawInteraction"), false, TEXT("Flags whether the interaction component should draw debug helpers in world."), ECVF_Cheat);
+
+UCInteractionComponent::UCInteractionComponent()
+{
+	FocusedActor = nullptr;
+	DefaultWidgetInstance = nullptr;
+
+	TraceDistance = 500;
+	TraceRadius = 30;
+	TraceFrequency = 0.15;
+
+	CollisionChannel = ECC_WorldDynamic;
+}
 
 // ReSharper disable once CppMemberFunctionMayBeConst ~ Incorrect suggestion
 void UCInteractionComponent::PrimaryInteract()
 {
-	TArray<FHitResult> HitResults;
+	if (FocusedActor) ICGameplayInterface::Execute_Interact(FocusedActor, Cast<APawn>(GetOwner()));
+}
 
+void UCInteractionComponent::BeginPlay()
+{
+	Super::BeginPlay();
+
+	DefaultWidgetInstance = CreateWidget<UCWorldUserWidget>(GetWorld(), DefaultWidgetClass);
+
+	FTimerHandle TimerHandle;
+	GetWorld()->GetTimerManager().SetTimer(TimerHandle, [this] { FindBestInteractable(); }, TraceFrequency, true);
+}
+
+/**
+ * Finds the best interactable actor based on the current player's view and performs necessary actions such as setting the focused actor and showing the relevant world UI.
+ *
+ * @note It was required during class to do a timer-based, player-owned solution to the interaction problem instead of having the actors in the world, report back to the player
+ * that they're interactable.
+ *
+ */
+void UCInteractionComponent::FindBestInteractable()
+{
 	FVector EyeLocation;
 	FRotator EyeRotation;
 	GetOwner()->GetActorEyesViewPoint(EyeLocation, EyeRotation);
-	FVector End = EyeLocation + EyeRotation.Vector() * 1000;
+	FVector LineEnd = EyeLocation + EyeRotation.Vector() * TraceDistance;
 
 	FCollisionObjectQueryParams ObjectQueryParams;
-	ObjectQueryParams.AddObjectTypesToQuery(ECC_WorldDynamic);
+	ObjectQueryParams.AddObjectTypesToQuery(CollisionChannel);
 
-	bool bBlockingHit = GetWorld()->SweepMultiByObjectType(HitResults, EyeLocation, End, FQuat::Identity, ObjectQueryParams, FCollisionShape::MakeSphere(30));
+	TArray<FHitResult> HitResults;
+	bool bIsBlockingHit = GetWorld()->SweepMultiByObjectType(HitResults, EyeLocation, LineEnd, FQuat::Identity, ObjectQueryParams, FCollisionShape::MakeSphere(TraceRadius));
+
+	FocusedActor = nullptr;
 
 	for (FHitResult SingleHit : HitResults)
 	{
 		if (AActor* HitActor = SingleHit.GetActor(); HitActor && HitActor->Implements<UCGameplayInterface>())
 		{
-			ICGameplayInterface::Execute_Interact(HitActor, Cast<APawn>(GetOwner()));
-
-			if (CVarDebugDrawInteraction.GetValueOnGameThread()) DrawDebugSphere(GetWorld(), SingleHit.Location, 30.0F, 32, bBlockingHit ? FColor::Green : FColor::Red, false, 1.0F);
+			FocusedActor = HitActor;
+			if (CVarDebugDrawInteraction.GetValueOnGameThread()) DrawDebugSphere(GetWorld(), SingleHit.Location, TraceRadius, 32, bIsBlockingHit ? FColor::Green : FColor::Red, false, 1.0F);
 			break;
 		}
 	}
 
-	if (CVarDebugDrawInteraction.GetValueOnGameThread()) DrawDebugLine(GetWorld(), EyeLocation, End, bBlockingHit ? FColor::Green : FColor::Red, false, 1.5, 0, 1.0F);
+	if (CVarDebugDrawInteraction.GetValueOnGameThread()) DrawDebugLine(GetWorld(), EyeLocation, LineEnd, bIsBlockingHit ? FColor::Green : FColor::Red, false, 1.5, 0, 1.0F);
+
+	SetWorldWidget();
+}
+
+/**
+ * Sets the single-instanced widget in the world based on the currently focused actor.
+ *
+ * @note The instance of the widget is created during BeginPlay. 
+ */
+void UCInteractionComponent::SetWorldWidget()
+{
+	if (FocusedActor)
+	{
+		DefaultWidgetInstance->AttachedActor = FocusedActor;
+		if (!DefaultWidgetInstance->IsInViewport()) DefaultWidgetInstance->AddToViewport();
+	}
+	else
+	{
+		if (DefaultWidgetInstance) DefaultWidgetInstance->RemoveFromParent();
+	}
 }
--- Source/unrealcourse/Public/Components/CInteractionComponent.h
+++ Source/unrealcourse/Public/Components/CInteractionComponent.h
@@ -4,11 +4,43 @@
 #include "Components/ActorComponent.h"
 #include "CInteractionComponent.generated.h"
 
+class UCWorldUserWidget;
+
 UCLASS(ClassGroup=(Custom))
 class UNREALCOURSE_API UCInteractionComponent : public UActorComponent
 {
 	GENERATED_BODY()
 
 public:
+	UCInteractionComponent();
+
 	void PrimaryInteract();
+
+private:
+	UPROPERTY()
+	AActor* FocusedActor;
+
+	UPROPERTY(EditDefaultsOnly, Category="UI")
+	TSubclassOf<UCWorldUserWidget> DefaultWidgetClass;
+
+	UPROPERTY(VisibleAnywhere, Category="UI")
+	UCWorldUserWidget* DefaultWidgetInstance;
+
+	UPROPERTY(EditDefaultsOnly, Category="Interaction Trace", meta=(Tooltip = "The trace's max distance from the Player's viewpont, in Unreal Units."))
+	float TraceDistance;
+
+	UPROPERTY(EditDefaultsOnly, Category="Interaction Trace", meta=(Tooltip = "The trace sweep sphere's radius, in Unreal Units."))
+	float TraceRadius;
+
+	UPROPERTY(EditDefaultsOnly, Category="Interaction Trace", meta=(Tooltip = "The trace's frequency in seconds. Decimals values are supported and encouraged."))
+	float TraceFrequency;
+
+	UPROPERTY(EditDefaultsOnly, Category="Interaction Trace", meta=(Tooltip = "The trace's collision channel to trace against."))
+	TEnumAsByte<ECollisionChannel> CollisionChannel;
+
+	virtual void BeginPlay() override;
+	
+	void FindBestInteractable();
+
+	void SetWorldWidget();
 };
